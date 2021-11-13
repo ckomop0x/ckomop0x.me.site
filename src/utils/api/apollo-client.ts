39e4f65db06db9225b37574cf15a9830155fc5fd
Pavel Klochkov <paul.klochkov@gmail.com>
2021-11-14 00:56:39 +0100

    Working on the test fix

--- src/utils/api/apollo-client.ts
+++ src/utils/api/apollo-client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 const isDev = process?.env?.NODE_ENV === 'development';
 const graphQLUrl = isDev
-  ? process.env.NEXT_PUBLIC_STRAPI_URL_LOCAL
-  : process.env.NEXT_PUBLIC_STRAPI_URL;
+  ? process.env.NEXT_PUBLIC_GRAPHQL_URL_LOCAL
+  : process.env.NEXT_PUBLIC_GRAPHQL_URL;
 
 const apolloClient = new ApolloClient({
   uri: graphQLUrl,
