edde3e857c9284d5faa533e6e5baccaf3ff71760
Pavel Klochkov <paul.klochkov@gmail.com>
2023-09-03 22:40:18 +0200

    Format files with prettier

--- src/queries/types/PostsPathQuery.ts
+++ src/queries/types/PostsPathQuery.ts
@@ -8,17 +8,17 @@
 // ====================================================
 
 export interface PostsPathQuery_posts_data_attributes {
-  __typename: "Post";
+  __typename: 'Post';
   slug: string;
 }
 
 export interface PostsPathQuery_posts_data {
-  __typename: "PostEntity";
+  __typename: 'PostEntity';
   attributes: PostsPathQuery_posts_data_attributes | null;
 }
 
 export interface PostsPathQuery_posts {
-  __typename: "PostEntityResponseCollection";
+  __typename: 'PostEntityResponseCollection';
   data: PostsPathQuery_posts_data[];
 }
 
