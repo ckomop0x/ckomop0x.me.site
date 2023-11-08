1acc9af9a3179e692238ce3ffc93a4878de4f8ff
Pavel Klochkov <paul.klochkov@gmail.com>
2023-11-08 23:27:56 +0100

    Update outdir

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://example.com',
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: true, // (optional),
+  outDir: 'out',
 };
