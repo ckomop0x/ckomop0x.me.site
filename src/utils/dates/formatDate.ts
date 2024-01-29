05878df903f7ae8f4990ce492ff6e3cec02a6d70
Pavel Klochkov <paul.klochkov@gmail.com>
2024-01-29 22:53:15 +0100

    Fix date-fns-3.3.1 ESM import

--- src/utils/dates/formatDate.ts
+++ src/utils/dates/formatDate.ts
@@ -1,8 +1,8 @@
-import { format } from 'date-fns';
-import ruLocale from 'date-fns/locale/ru';
+import { format } from 'date-fns/format';
+import { ru } from 'date-fns/locale';
 
 export default function formatDate(date: string | Date): string {
   return format(new Date(date), 'dd MMMM yyyy', {
-    locale: ruLocale,
+    locale: ru,
   });
 }
