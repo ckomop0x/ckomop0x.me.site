d2bef2623e61977311f6bcaf62279e8e9f521f13
Pavel Klochkov <ckomop0x@users.noreply.github.com>
2025-03-15 01:28:19 +0100

    Fix/sitemap issue fix (#1141)
    This pull request includes several changes to the configuration and
    package files to update the site version, rename the sitemap
    configuration file, and adjust the postbuild script. The most important
    changes are listed below:
    
    Configuration updates:
    
    *
    [`next-sitemap.config.mjs`](diffhunk://#diff-ffb7932b783a758aad8669b88ffb446e515ce5599266bf5eee614fabdc043081L2-R5):
    Renamed from `next-sitemap.config.js` and updated the export to ES
    module syntax. Added exclusion of specific paths from the sitemap
    generation.
    
    Package updates:
    
    *
    [`package.json`](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519L3-R3):
    Updated the site version from `7.1.6` to `7.1.7`.
    *
    [`package.json`](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519L26-R26):
    Modified the `postbuild` script to use the renamed sitemap configuration
    file `next-sitemap.config.mjs`.

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
+export default {
   siteUrl: process.env.SITE_URL || 'https://ckomop0x.me',
   generateRobotsTxt: true, // (optional),
+  exclude: ['/poetry-test', '/test'], // (optional),
   outDir: './out',
 };
