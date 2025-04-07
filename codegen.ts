4d1f7b14876d629c2728818153b5015fa767fa27
Pavel Klochkov <paul.klochkov@gmail.com>
2025-04-07 15:34:00 +0200

    Explicitly verify NEXT_PUBLIC_URL_LOCAL before constructing the URL

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,9 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 import * as dotenv from 'dotenv';
 
-const schemaUrl =
-  `${dotenv.config().parsed?.NEXT_PUBLIC_URL_LOCAL}/graphql` || '';
+const schemaUrl = dotenv.config().parsed?.NEXT_PUBLIC_URL_LOCAL
+  ? `${dotenv.config().parsed.NEXT_PUBLIC_URL_LOCAL}/graphql`
+  : '';
 
 const config: CodegenConfig = {
   overwrite: true,
